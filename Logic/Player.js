258bfd52b4b5419496db255e35ad4ee7ed12280b
livinglemming <78166963+brownmouse10@users.noreply.github.com>
2022-08-20 14:55:38 +0200

    Player.js

---
+++ Logic/Player.js
@@ -0,0 +1,70 @@
+// If you dont know what something means, dont change it :)
+class Players {
+  // Player Data
+  let HighID = 0;
+  let LowID = 0;
+  let Token = "TOKEN";
+  let BoxID = 0;
+  let MapID = 0;
+  let SlotIndex = 0;
+  let RoomID = 0;
+  let BrawlerID;
+  let SkinsID;
+  let CardSkillsID;
+  let CardUnlockID;
+  let BrawlerUnlockedState = {};
+
+  // Brawler Data
+  SkinsID = Files.getSkinsID();
+  BrawlerID = Files.getCharactersID();
+  CardSkillsID = Files.getSpgID();
+  CardUnlockID = Files.getBrawlerUnlock();
+
+  ///////////////////////
+  // Configurable Data //
+  let Name = "LivingLemming";
+  let NameColour = 0;
+  let DoNotDisturb = 0;
+  let UnlockType = "All";
+  
+  // Resources
+  let PlayerEXP = 99999;
+  let PlayerTrophies = 0;
+  let Gems = 0;
+  let Gold = 0;
+  let MaxPlayerTrophiesGained = 0;
+  let StarPoints = 0;
+  let Tickets = 0;
+  let Tokens = 0;
+
+  // Boxes
+  let BrawlBoxes = 0;
+  let BigBoxes = 0;
+
+  let Region = "CZ";
+  let ContentCreator = "LivingLemming";
+
+  if UnlockType == "All" {
+      for (let i = 0; i in BrawlerID; i++) {
+          BrawlerUnlockedState[i.toString] = 1;
+      }
+  }
+
+  let ErrorCode;
+  let Maintenence = false;
+  let MaintenenceTime = 0;
+
+  let BattleResult = 0;
+  let GameType = 0;
+
+  bot1 = 0;
+  bot1n = null;
+  bot2 = 0;
+  bot2n = null;
+  bot3 = 0;
+  bot3n = null;
+  bot4 = 0;
+  bot4n = null;
+  bot5 = 0;
+  bot5n = null;
+}
