5fb98b930718f66de181474c86f7a9cf9c8370f7
livinglemming <78166963+brownmouse10@users.noreply.github.com>
2022-08-20 16:09:25 +0200

    Update OwnHomeDataMessage.js

--- Packets/Messages/Server/OwnHomeDataMessage.js
+++ Packets/Messages/Server/OwnHomeDataMessage.js
@@ -1,36 +1,59 @@
 const ByteStream = require("../../../ByteStream");
 const Messaging = require("../../../ByteStream/Messaging");
-
-let PlayerTrophies = 0;
-let MaxPlayerTrophiesReached = 0;
-let PlayerEXP = 99999;
+const Player = require("../../../Logic/Player");
 
 module.exports = {
     id: 20100,
     send: function(client){
         let buffer = new ByteStream();
+        let player = new Player();
 
         buffer.writeVInt(0);
         buffer.writeVInt(0);
       
-        buffer.writeVInt(PlayerTrophies); // Player Trophies
-        buffer.writeVInt(MaxPlayerTrophiesReached); // Player Max Reached Trophies
+        buffer.writeVInt(player.PlayerTrophies); // Player Trophies
+        buffer.writeVInt(player.MaxPlayerTrophiesGained); // Player Max Reached Trophies
       
         buffer.writeVInt(0);
         buffer.writeVInt(95); // Trophy Road Reward
       
-        buffer.writeVInt(PlayerEXP); // Player EXP
+        buffer.writeVInt(player.PlayerEXP); // Player EXP
       
         buffer.writeScId(28, 0); // Player Icon ID
-        buffer.writeScId(43, 0); // Player Name Colour ID
+        buffer.writeScId(43, player.NameColour); // Player Name Colour ID
         
         buffer.writeVInt(0);
         
-        // Selected Skins
-        buffer.writeVInt(brawler_skins.length)
-        for (brawler_id in brawler_skins) {
-            
-        }
+        // Selected Skins Array
+        buffer.writeVInt(player.BrawlerSkins.length);
+        // Unlocked Skins Array
+        buffer.writeVInt(player.SkinsID.length);
+        
+        buffer.writeVInt(0); // Leaderboard Global TID (Asia, Global)
+        buffer.writeVInt(0);
+        buffer.writeVInt(0);
+        
+        buffer.writeBoolean(false); // Token Limit Reached message if true
+        buffer.writeVInt(0);
+        buffer.writeBoolean(true);
+        
+        buffer.writeVInt(player.TokensDoubler);
+        buffer.writeVInt(1209599); // Season End Timer
+        buffer.writeVInt(0);
+        buffer.writeVInt(0);
+        
+        buffer.writeVInt(0);
+        buffer.writeVInt(0);
+        
+        buffer.writeVInt(8);
+        
+        buffer.writeBoolean(true);
+        buffer.writeBoolean(true);
+        buffer.writeBoolean(true);
+        
+        buffer.writeVInt(player.NameChangePrice);
+        buffer.writeVInt(player.NameChangeCooldownTimer);
+        
         // Work in progress...
 
         let message = new Messaging(client.client);
