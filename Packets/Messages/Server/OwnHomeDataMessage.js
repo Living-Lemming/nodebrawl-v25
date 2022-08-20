367ad00559ae805e42984119b6788c1c5a2a5482
livinglemming <78166963+brownmouse10@users.noreply.github.com>
2022-08-20 10:58:33 +0200

    Work in progress OHD...

--- Packets/Messages/Server/OwnHomeDataMessage.js
+++ Packets/Messages/Server/OwnHomeDataMessage.js
@@ -1,6 +1,10 @@
 const ByteStream = require("../../../ByteStream");
 const Messaging = require("../../../ByteStream/Messaging");
 
+let PlayerTrophies = 0;
+let MaxPlayerTrophiesReached = 0;
+let PlayerEXP = 99999;
+
 module.exports = {
     id: 20100,
     send: function(client){
@@ -9,16 +13,25 @@ module.exports = {
         buffer.writeVInt(0);
         buffer.writeVInt(0);
       
-        buffer.writeVInt(0); // Player Trophies
-        buffer.writeVInt(0); // Player Max Reached Trophies
+        buffer.writeVInt(PlayerTrophies); // Player Trophies
+        buffer.writeVInt(MaxPlayerTrophiesReached); // Player Max Reached Trophies
       
         buffer.writeVInt(0);
         buffer.writeVInt(95); // Trophy Road Reward
       
-        buffer.writeVInt(99999); // Player EXP
+        buffer.writeVInt(PlayerEXP); // Player EXP
       
-        buffer.writeVInt(28); // Player Icon ID
-        // In progres...
+        buffer.writeScId(28, 0); // Player Icon ID
+        buffer.writeScId(43, 0); // Player Name Colour ID
+        
+        buffer.writeVInt(0);
+        
+        // Selected Skins
+        buffer.writeVInt(brawler_skins.length)
+        for (brawler_id in brawler_skins) {
+            
+        }
+        // Work in progress...
 
         let message = new Messaging(client.client);
         message.send({
