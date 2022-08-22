f056a917b2ee6f0abff4394ab4b212464b1826ed
livinglemming <78166963+brownmouse10@users.noreply.github.com>
2022-08-22 12:54:41 +0200

    Update LoginMessage.js

--- Packets/Messages/Client/LoginMessage.js
+++ Packets/Messages/Client/LoginMessage.js
@@ -1,11 +1,13 @@
 const ByteStream = require("../../../ByteStream");
 const LoginOkMessage = require("../Server/LoginOkMessage");
 const OwnHomeDataMessage = require("../Server/OwnHomeDataMessage");
+const Player = require("../../../Logic/Player");
 
 module.exports = {
     id: 10101,
     handle: function(client){
         let data = new ByteStream(client.payload);
+        let player = new Player()
         LoginOkMessage.send(client);
         OwnHomeDataMessage.send(client);
     }
