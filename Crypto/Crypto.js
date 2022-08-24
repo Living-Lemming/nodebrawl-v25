e4e8356c2877f6d6d6b975b6d21175541195b144
livinglemming <78166963+brownmouse10@users.noreply.github.com>
2022-08-24 22:42:14 +0200

    Update Crypto.js

--- Crypto/Crypto.js
+++ Crypto/Crypto.js
@@ -1,3 +1,1 @@
-class Crypto {
-    
-}
+import { crypto_box_afternm, crypto_box_beforenm, crypto_scalarmult_base, crypto_box_open_afternm, crypto_box_NONCEBYTES, crypto_hash, crypto_secretbox, crypto_secretbox_open } from "tweetnacl";
