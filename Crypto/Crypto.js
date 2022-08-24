77a4c74f59b3be27db48b7a2981cf801a2ecc992
livinglemming <78166963+brownmouse10@users.noreply.github.com>
2022-08-24 22:51:33 +0200

    Update Crypto.js

--- Crypto/Crypto.js
+++ Crypto/Crypto.js
@@ -1,1 +1,7 @@
 import { crypto_box_afternm, crypto_box_beforenm, crypto_scalarmult_base, crypto_box_open_afternm, crypto_box_NONCEBYTES, crypto_hash, crypto_secretbox, crypto_secretbox_open } from "tweetnacl";
+
+let ServerKey = bytes.from_hex("key");
+let ClientSK = bytes.from_hex("key");
+let ClientPK = crypto_scalarmult_base(ClientSK);
+let SessionKey = Buffer.from("key", "hex");
+
