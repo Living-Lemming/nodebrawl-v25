b608541a964133e8543673bcd2c0be5aa671abae
livinglemming <78166963+brownmouse10@users.noreply.github.com>
2022-09-11 16:19:21 +0200

    Update Player.js

--- Logic/Player.js
+++ Logic/Player.js
@@ -1,96 +1,93 @@
 // If you dont know what something means, dont change it :)
-class Players {
-  // Player Data
-  HighID = 0;
-  LowID = 0;
-  Token = "TOKEN";
-  BoxID = 0;
-  MapID = 0;
-  SlotIndex = 0;
-  RoomID = 0;
-  BrawlerID;
-  SkinsID;
-  CardSkillsID;
-  CardUnlockID;
-  BrawlerUnlockedState = {};
+// Player Data
+let HighID = 0;
+let LowID = 0;
+let Token = "TOKEN";
+let BoxID = 0;
+let MapID = 0;
+let SlotIndex = 0;
+let RoomID = 0;
+let BrawlerID;
+let SkinsID;
+let CardSkillsID;
+let CardUnlockID;
+let BrawlerUnlockedState = {};
     
-  // Brawler Data
-  SkinsID = Files.getSkinsID();
-  BrawlerID = Files.getCharactersID();
-  CardSkillsID = Files.getSpgID();
-  CardUnlockID = Files.getBrawlerUnlock();
+// Brawler Data
+let SkinsID = Files.getSkinsID();
+let BrawlerID = Files.getCharactersID();
+let CardSkillsID = Files.getSpgID();
+let CardUnlockID = Files.getBrawlerUnlock();
 
-  ///////////////////////
-  // Configurable Data //
-  Name = "LivingLemming";
-  NameColour = 0;
-  DoNotDisturb = 0;
-  UnlockType = "All";
+///////////////////////
+// Configurable Data //
+let Name = "LivingLemming";
+let NameColour = 0;
+let DoNotDisturb = 0;
+let UnlockType = "All";
   
-  // Resources
-  PlayerEXP = 99999;
-  TokensDoubler = 0;
-  PlayerTrophies = 0;
-  Gems = 0;
-  Gold = 0;
-  MaxPlayerTrophiesGained = 0;
-  StarPoints = 0;
-  Tickets = 0;
-  Tokens = 0;
+// Resources
+let PlayerEXP = 99999;
+let TokensDoubler = 0;
+let PlayerTrophies = 0;
+let Gems = 0;
+let Gold = 0;
+let MaxPlayerTrophiesGained = 0;
+let StarPoints = 0;
+let Tickets = 0;
+let Tokens = 0;
 
-  // Boxes
-  BrawlBoxes = 0;
-  TokensNeededForBrawlBox = 100;
-  BigBoxes = 0;
-  StarTokensNeededForBigBox = 10;
+// Boxes
+let BrawlBoxes = 0;
+let TokensNeededForBrawlBox = 100;
+let BigBoxes = 0;
+let StarTokensNeededForBigBox = 10;
 
-  Region = "CZ";
-  ContentCreator = "LivingLemming";
+let Region = "CZ";
+let ContentCreator = "LivingLemming";
 
-  NameChangePrice = 0;
-  NameChangeCooldownTimer = 0;
+let NameChangePrice = 0;
+let NameChangeCooldownTimer = 0;
 
-  BrawlerPowerLevel = 1;
-  BrawlerTrophiesForRank = 0;
-  BrawlerTrophies = 0;
-  BrawlerUpgradePoints = 0;
+let BrawlerPowerLevel = 1;
+let BrawlerTrophiesForRank = 0;
+let BrawlerTrophies = 0;
+let BrawlerUpgradePoints = 0;
 
-  BannedIPs = {
-      01 : "",
-      02 : "",
-      03 : ""
-  };
+let BannedIPs = {
+    01 : "",
+    02 : "",
+    03 : ""
+};
 
-  if (UnlockType == "All") {
-      for (i = 0; i in BrawlerID; i++) {
-          BrawlerUnlockedState[i.toString] = 1;
-      }
-  }
-
-  BrawlerSpgUnlock = {};
-  starpower = 76
-
-  BrawlerTrophies = {};
-  BrawlerTrophies.forEach(element => BrawlerTrophies.concat({element.toString(): BrawlerTrophiesForRank}));
-
-  BrawlerSkins = {};
-  BrawlerSkins.forEach(element => BrawlerSkins.concat({element.toString(): 0}));
-
-  ErrorCode;
-  Maintenence = false;
-
-  BattleResult = 0;
-  GameType = 0;
-
-  bot1 = 0;
-  bot1n = null;
-  bot2 = 0;
-  bot2n = null;
-  bot3 = 0;
-  bot3n = null;
-  bot4 = 0;
-  bot4n = null;
-  bot5 = 0;
-  bot5n = null;
+if (UnlockType == "All") {
+    for (i = 0; i in BrawlerID; i++) {
+        BrawlerUnlockedState[i.toString] = 1;
+    }
 }
 
+let BrawlerSpgUnlock = {};
+let starpower = 76
+
+let BrawlerTrophies = {};
+BrawlerTrophies.forEach(element => BrawlerTrophies.concat({element.toString(): BrawlerTrophiesForRank}));
+
+let BrawlerSkins = {};
+BrawlerSkins.forEach(element => BrawlerSkins.concat({element.toString(): 0}));
+
+let ErrorCode;
+let Maintenence = false;
+
+let BattleResult = 0;
+let GameType = 0;
+
+let bot1 = 0;
+let bot1n = null;
+let bot2 = 0;
+let bot2n = null;
+let bot3 = 0;
+let bot3n = null;
+let bot4 = 0;
+let bot4n = null;
+let bot5 = 0;
+let bot5n = null;
