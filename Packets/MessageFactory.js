9d47ac24355a9348445e50252d502485cbe2ab80
LivingLemming <78166963+brownmouse10@users.noreply.github.com>
2022-09-18 09:56:01 +0200

    Update MessageFactory.js

--- Packets/MessageFactory.js
+++ Packets/MessageFactory.js
@@ -2,15 +2,18 @@ const fs = require("fs");
 
 class MessageFactory{
     constructor(){
-        this.packets = {};
-        fs.readdir('./Packets/Messages/Client', (err, files) => {
+        this.packets = {}
+
+        fs.readdir('./Protocol/Messages/Client', (err, files) => {
+            if(err)console.log(err)
             files.forEach(e => {
-                let packet = require(`./Messages/Client/${e.replace(".js", "")}`);
-        
-                this.packets[packet.id] = packet.handle 
+                let Packet = require(`./Messages/Client/${e.replace(".js", "")}`);
+                let packetClass = new Packet()
+
+                this.packets[packetClass.id] = Packet 
             })
         })
-    };
+    }
 
     handle(id){
         return this.packets[id]
@@ -19,6 +22,7 @@ class MessageFactory{
     getPackets(){
         return Object.keys(this.packets)
     }
-};
+
+}
 
 module.exports = MessageFactory
