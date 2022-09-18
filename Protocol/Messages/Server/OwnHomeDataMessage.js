ca777d790b5f39488d10125d1040259a2decfb41
LivingLemming <78166963+brownmouse10@users.noreply.github.com>
2022-09-18 10:35:44 +0200

    Update OwnHomeDataMessage.js

--- Protocol/Messages/Server/OwnHomeDataMessage.js
+++ Protocol/Messages/Server/OwnHomeDataMessage.js
@@ -1,116 +1,107 @@
-const ByteStream = require("../../../ByteStream");
-const Messaging = require("../../../ByteStream/Messaging");
-const Player = require("../../../Logic/Player");
+class OwnHomeDataMessage {
+    constructor(client, player, bytes){
+        super(client, bytes)
+        this.client = client
+        this.player = player
+        this.id = 20100
+        this.version = 0
+    }
 
-module.exports = {
-    id: 20100,
-    send: function(client){
-        let buffer = new ByteStream();
-        let player = new Player();
-
-        buffer.writeVInt(0);
-        buffer.writeVInt(0);
+    encode(){
+        this.writeVInt(0);
+        this.writeVInt(0);
       
-        buffer.writeVInt(player.PlayerTrophies); // Player Trophies
-        buffer.writeVInt(player.MaxPlayerTrophiesGained); // Player Max Reached Trophies
+        this.writeVInt(player.PlayerTrophies); // Player Trophies
+        this.writeVInt(player.MaxPlayerTrophiesGained); // Player Max Reached Trophies
       
-        buffer.writeVInt(0);
-        buffer.writeVInt(95); // Trophy Road Reward
+        this.writeVInt(0);
+        this.writeVInt(95); // Trophy Road Reward
       
-        buffer.writeVInt(player.PlayerEXP); // Player EXP
+        this.writeVInt(player.PlayerEXP); // Player EXP
       
-        buffer.writeScId(28, 0); // Player Icon ID
-        buffer.writeScId(43, player.NameColour); // Player Name Colour ID
+        this.writeScId(28, 0); // Player Icon ID
+        this.writeScId(43, player.NameColour); // Player Name Colour ID
         
-        buffer.writeVInt(0);
+        this.writeVInt(0);
         
         // Selected Skins Array
-        buffer.writeVInt(player.BrawlerSkins.length);
-        player.BrawlerSkins.forEach(element => buffer.writeScId(29, player.BrawlerSkins[element]);
+        this.writeVInt(player.BrawlerSkins.length);
+        this.BrawlerSkins.forEach(element => buffer.writeScId(29, player.BrawlerSkins[element]);
         
         // Unlocked Skins Array
-        buffer.writeVInt(player.SkinsID.length);
-        player.SkinsID.forEach(element => buffer.writeScId(29, element);
+        this.writeVInt(player.SkinsID.length);
+        this.player.SkinsID.forEach(element => buffer.writeScId(29, element);
         
-        buffer.writeVInt(0); // Leaderboard Global TID (Asia, Global)
-        buffer.writeVInt(0);
-        buffer.writeVInt(0);
+        this.writeVInt(0); // Leaderboard Global TID (Asia, Global)
+        this.writeVInt(0);
+        this.writeVInt(0);
         
-        buffer.writeBoolean(false); // Token Limit Reached message if true
-        buffer.writeVInt(0);
-        buffer.writeBoolean(true);
+        this.writeBoolean(false); // Token Limit Reached message if true
+        this.writeVInt(0);
+        this.writeBoolean(true);
         
-        buffer.writeVInt(player.TokensDoubler);
-        buffer.writeVInt(1209599); // Season End Timer
-        buffer.writeVInt(0);
-        buffer.writeVInt(0);
+        this.writeVInt(player.TokensDoubler);
+        this.writeVInt(1209599); // Season End Timer
+        this.writeVInt(0);
+        this.writeVInt(0);
         
-        buffer.writeVInt(0);
-        buffer.writeVInt(0);
+        this.writeVInt(0);
+        this.writeVInt(0);
         
-        buffer.writeVInt(8);
+        this.writeVInt(8);
         
-        buffer.writeBoolean(true);
-        buffer.writeBoolean(true);
-        buffer.writeBoolean(true);
+        this.writeBoolean(true);
+        this.writeBoolean(true);
+        this.writeBoolean(true);
         
-        buffer.writeVInt(player.NameChangePrice);
-        buffer.writeVInt(player.NameChangeCooldownTimer);
+        this.writeVInt(player.NameChangePrice);
+        this.writeVInt(player.NameChangeCooldownTimer);
         
-        let count = Shop.offers.length;
-        buffer.writeVInt(count);
-        count.forEach(function () {
-            
-        });
+        this.writeVInt(0);
         
-        buffer.writeVInt(0);
+        this.writeVInt(0);
         
-        buffer.writeVInt(200);
-        buffer.writeVInt(0); // Time till bonus tokens
+        this.writeVInt(200);
+        this.writeVInt(0); // Time till bonus tokens
         
-        buffer.writeVInt(0);
+        this.writeVInt(0);
         
-        buffer.writeVInt(player.Tickets);
-        buffer.writeVInt(0);
+        this.writeVInt(player.Tickets);
+        this.writeVInt(0);
         
-        buffer.writeScId(16, player.BrawlerID);
+        this.writeScId(16, player.BrawlerID);
         
-        buffer.writeString(player.Region);
-        buffer.writeString(player.ContentCreator);
+        this.writeString(player.Region);
+        this.writeString(player.ContentCreator);
         
-        buffer.writeVInt(0);
-        buffer.writeVInt(0);
-        buffer.writeVInt(0);
-        buffer.writeVInt(0);
-        buffer.writeVInt(0);
+        this.writeVInt(0);
+        this.writeVInt(0);
+        this.writeVInt(0);
+        this.writeVInt(0);
+        this.writeVInt(0);
         
-        buffer.writeVInt(2019049);
+        this.writeVInt(2019049);
         
-        buffer.writeVInt(player.TokensNeededForBrawlBox);
-        buffer.writeVInt(player.StarTokensNeededForBigBox);
+        this.writeVInt(player.TokensNeededForBrawlBox);
+        this.writeVInt(player.StarTokensNeededForBigBox);
         
         /*
         for item in Shop.boxes:
             self.writeVint(item['Cost'])
             self.writeVint(item['Multiplier'])
-        
-        self.writeVint(Shop.token_doubler['Cost'])
-        self.writeVint(Shop.token_doubler['Amount'])
         */
         
-        buffer.writeVInt(500);
-        buffer.writeVInt(50);
-        buffer.writeVInt(999900);
+        this.writeVInt(0); // self.writeVint(Shop.token_doubler['Cost'])
+        this.writeVInt(0); // self.writeVint(Shop.token_doubler['Amount'])
         
-        buffer.writeVInt(0);
+        this.writeVInt(500);
+        this.writeVInt(50);
+        this.writeVInt(999900);
         
-        buffer.writeVInt(20);
-
-        let message = new Messaging(client.client);
-        message.send({
-            id: this.id,
-            buffer,
-            version: client.version
-        })
+        this.writeVInt(0);
+        
+        this.writeVInt(20);
     }
 }
+
+module.exports = OwnHomeDataMessage
