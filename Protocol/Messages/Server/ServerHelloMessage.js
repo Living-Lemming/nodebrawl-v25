5ce498c09faf320255ea0cda0a4fc9bcc2b82234
LivingLemming <78166963+brownmouse10@users.noreply.github.com>
2022-09-18 10:29:30 +0200

    Update ServerHelloMessage.js

--- Protocol/Messages/Server/ServerHelloMessage.js
+++ Protocol/Messages/Server/ServerHelloMessage.js
@@ -1,44 +1,13 @@
-const ByteStream = require("../../../ByteStream");
-const Messaging = require("../../../ByteStream/Messaging");
+class ServerHelloMessage {
+    constructor(client, bytes) {
+        super(client, bytes)
+        this.id = 20100
+        this.version = 0
+    }
 
-module.exports = {
-    id: 20100,
-    send: function(client){
-        let buffer = new ByteStream();
-
-        buffer.writeInt(1);
-      
-        buffer.writeString("");
-      
-        buffer.writeString();
-        
-        buffer.writeString("");
-        buffer.writeString("");
-        buffer.writeString("Crypto Not Disabled...");
-      
-        buffer.writeInt(0);
-        buffer.writeBoolean(false);
-      
-        buffer.writeString();
-        buffer.writeString();
-      
-        buffer.writeInt(0);
-        buffer.writeInt(3);
-      
-        buffer.writeString();
-        buffer.writeString();
-      
-        buffer.writeInt(0);
-        buffer.writeInt(0);
-      
-        buffer.writeBoolean(false);
-        buffer.writeBoolean(false);
-
-        let message = new Messaging(client.client);
-        message.send({
-            id: this.id,
-            buffer,
-            version: client.version
-        })
+    encode() {
+        this.writeInt(0);
     }
 }
+
+module.exports = ServerHelloMessage
