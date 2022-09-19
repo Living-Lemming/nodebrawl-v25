a21b070df49f61c273b92d4d4b0204ca84c2a1fe
LivingLemming <78166963+brownmouse10@users.noreply.github.com>
2022-09-19 08:54:30 +0200

    small update

--- Protocol/Messages/Server/LoginFailedMessage.js
+++ Protocol/Messages/Server/LoginFailedMessage.js
@@ -1,11 +1,27 @@
-class LoginFailedMessage {
-    constructor(client, player, bytes){
+const PiranhaMessage = require('../../PiranhaMessage')
+
+class LoginFailedMessage extends PiranhaMessage {
+    constructor(client, player, bytes, msg){
         super(client, bytes)
         this.client = client
         this.player = player
-        this.id = 20100
+        this.msg = msg
+        this.id = 20103
         this.version = 0
     }
+
+    /*
+        << Error Code List >>
+        # 1  = Custom Message
+        # 7  = Patch
+        # 8  = Update Available
+        # 9  = Failed to Connect
+        # 10 = Maintenance
+        # 11 = Banned
+        # 13 = Acc Locked PopUp
+        # 16 = Updating Cr/Maintenance
+        # 18 = Chinese Text?
+    */
 
     encode(){
         this.writeInt(this.player.ErrorCode);
@@ -15,7 +31,7 @@ class LoginFailedMessage {
         
         this.writeString('');
         this.writeString('');
-        this.writeString('');
+        this.writeString(this.msg);
         
         this.writeInt(0);
         this.writeBoolean(false);
@@ -38,3 +54,4 @@ class LoginFailedMessage {
 }
 
 module.exports = LoginFailedMessage
+
