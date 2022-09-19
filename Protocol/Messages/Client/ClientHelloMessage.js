933822ad42d9bb70263a5312fd2a9016c43f8b00
LivingLemming <78166963+brownmouse10@users.noreply.github.com>
2022-09-19 08:56:26 +0200

    Update ClientHelloMessage.js

--- Protocol/Messages/Client/ClientHelloMessage.js
+++ Protocol/Messages/Client/ClientHelloMessage.js
@@ -1,10 +1,11 @@
-const ServerHelloMessage = new (require("../Server/ServerHelloMessage"));
 const PiranhaMessage = require("../../PiranhaMessage");
+const LoginFailedMessage = new (require('../Server/LoginFailedMessage'))()
 
 class ClientHelloMessage extends PiranhaMessage {
-    constructor(client, bytes) {
+    constructor(client, player, bytes) {
         super(bytes)
         this.client = client;
+        this.player = player
         this.id = 10100
         this.version = 0
     }
@@ -14,7 +15,8 @@ class ClientHelloMessage extends PiranhaMessage {
     }
 
     process() {
-        ServerHelloMessage.send(this.client)
+        this.player.ErrorCode = 1
+        LoginFailedMessage.send(this.client, this.player, bytes, 'Crypto Not Disabled')
     }
 }
 
