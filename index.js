668010f989bb8d05ca667441b8c6b60ef7b6ccd4
LivingLemming <78166963+Living-Lemming@users.noreply.github.com>
2023-01-03 09:11:31 +0100

    Core Update [v2.2]

--- index.js
+++ index.js
@@ -1,62 +1,59 @@
-const net = require("net");
-const Packetizer = require("./ByteStream/packetizer");
-const MessageFactory = require("./Protocol/MessageFactory");
-const server = new net.Server();
-const Messages = new MessageFactory();
+const net = require('net')
+const MessageFactory = require('./Protocol/MessageFactory')
+const server = new net.Server()
+const Messages = new MessageFactory()
 
-const PORT = 9339;
+const PORT = 9339
 
-server.on("connection", async(client) => {
-    client.log = function(text){
-        return console.log(`[${this.remoteAddress.split(":").slice(-1)}] >> ${text}`)
-    };
+server.on('connection', async (client) => {
+  client.log = function (text) {
+    return console.log(`[${this.remoteAddress.split(':').slice(-1)}] >> ${text}`)
+  }
 
-    client.log(`A wild connection appeard!`);
-    const packets = Messages.getPackets();
-    const packetizer = new Packetizer();
+  client.log('A wild connection appeard!')
+  const packets = Messages.getPackets()
 
+  client.on('data', async (packet) => {
+    const message = {
+      id: packet.readUInt16BE(0),
+      len: packet.readUIntBE(2, 3),
+      version: packet.readUInt16BE(5),
+      payload: packet.slice(7, this.len),
+      client,
+    }
+    if (packets.indexOf(String(message.id)) !== -1) {
+      try {
+        const packet = new (Messages.handle(message.id))(client, message.payload)
 
-    client.on('data', async(chunk) => {
-        packetizer.packetize(chunk, (packet) => {
-            let message = {
-                id: packet.readUInt16BE(0),
-                len: packet.readUIntBE(2, 3),
-                version: packet.readUInt16BE(5),
-                payload: packet.slice(7, this.len),
-                client
-            };
-            if(packets.indexOf(String(message.id)) != -1){
-                try{
-                    const packet = new (Messages.handle(message.id))(client, message.payload);
+        client.log(`[PACKET] Gotcha ${message.id} (${packet.constructor.name})! `)
 
-                    client.log(`Gotcha ${message.id} (${packet.constructor.name}) packet! `);
-                    
-                    packet.decode();
-                    packet.process()
+        packet.decode()
+        packet.process()
+      } catch (e) {
+        console.log(e)
+      }
+    } else {
+      client.log(`[PACKET] Gotcha undefined ${message.id}!`)
+    }
+  })
 
-                }catch(e){
-                    console.log(e)
-                }
-            }else{
-                client.log(`Gotcha undefined ${message.id} packet!`)
-            }
-        })
-    });
+  client.on('end', async () => {
+    return client.log('Client disconnected.')
+  })
 
-    client.on('end', async () => {
-        return client.log(`Client disconnected.`)
-    });
-
-    client.on('error', async error => {
-        try {
-            client.log(`A wild error!`);
-            console.log(error);
-            client.destroy()
-        } catch (e) { }
-    })
-
+  client.on('error', async error => {
+    try {
+      client.log('A wild error!')
+      console.log(error)
+      client.destroy()
+    } catch (e) { }
+  })
 })
 
-
-server.once('listening', () => console.log(`Server started on ${PORT} port!`));
+server.once('listening', () => console.log(`Server started on ${PORT} port!`))
 server.listen(PORT)
+
+
+process.on("uncaughtException", e => console.log(e));
+
+process.on("unhandledRejection", e => console.log(e));
